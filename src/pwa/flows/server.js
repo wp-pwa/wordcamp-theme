03712c42385dbb9aeedfec936e23daf9c74ef561
David Arenas <darensr@gmail.com>
2018-05-14 12:37:36 +0200

    Map speaker ids by session using a middleware

--- src/pwa/flows/server.js
+++ src/pwa/flows/server.js
@@ -1,17 +1,65 @@
-import { flow, getEnv } from 'mobx-state-tree';
+import { flow, getEnv, addMiddleware } from 'mobx-state-tree';
 import { when } from 'mobx';
 import { dep } from 'worona-deps';
 import { homeContext } from '../contexts';
 
+const extractId = href => /\/(\d+)$/g.exec(href)[1];
+
+const speakersMiddleware = (call, next, abort) => {
+  if (call.name === 'addEntity') {
+    const [{ entity }] = call.args;
+
+    if (entity.type === 'wcb_session') {
+      const { theme } = call.tree;
+      const { id, _links: { speakers = [] } } = entity;
+      theme.addSpeakersBySession(id.toString(), speakers.map(({ href }) => extractId(href)));
+    }
+  }
+  next(call);
+};
+
 export default self =>
   flow(function* ThemeServer({ selectedItem }) {
+    addMiddleware(self, speakersMiddleware);
+
     const { store } = getEnv(self);
     const routeChangeSucceed = dep('connection', 'actions', 'routeChangeSucceed');
+    const customRequested = dep('connection', 'actions', 'customRequested');
 
     const { type, id } = selectedItem;
     const action = { selectedItem: { type, id }, context: homeContext };
 
     store.dispatch(routeChangeSucceed(action));
 
-    yield when(() => self.connection.entity(type, id).isReady);
+    // Request all sessions:
+    store.dispatch(
+      customRequested({
+        custom: { name: 'sessions', type: 'wcb_session', page: 1 },
+        params: { per_page: 100, _embed: false },
+      }),
+    );
+
+    // Request all speakers:
+    store.dispatch(
+      customRequested({
+        custom: { name: 'speakers', type: 'wcb_speaker', page: 1 },
+        params: { per_page: 100, _embed: false },
+      }),
+    );
+
+    // Request all tracks:
+    store.dispatch(
+      customRequested({
+        custom: { name: 'tracks', type: 'wcb_track', page: 1 },
+        params: { per_page: 100, _embed: false },
+      }),
+    );
+
+    yield when(
+      () =>
+        self.connection.entity(type, id).isReady &&
+        self.connection.custom('sessions').isReady &&
+        self.connection.custom('speakers').isReady &&
+        self.connection.custom('tracks').isReady,
+    );
   });
--- src/pwa/stores/index.js
+++ src/pwa/stores/index.js
@@ -1,3 +1,24 @@
-import { types } from 'mobx-state-tree';
+import { types, getParent } from 'mobx-state-tree';
 
-export default types.model('Wordcamp').props({});
+const Id = types.union(types.number, types.string);
+
+export default types
+  .model('Wordcamp')
+  .props({
+    speakersBySession: types.optional(types.map(types.array(types.maybe(Id))), {}),
+  })
+  .views(self => {
+    const getConnection = () => getParent(self).connection;
+    return {
+      getSpeakersBySession(sessionId) {
+        return self.speakersBySession
+          .get(sessionId.toString())
+          .map(speakerId => getConnection().entity('wcb_speaker', speakerId));
+      },
+    };
+  })
+  .actions(self => ({
+    addSpeakersBySession(sessionId, speakerIds) {
+      self.speakersBySession.set(sessionId, speakerIds);
+    },
+  }));
