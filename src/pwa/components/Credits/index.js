c561a4a418baad738f5414a1b0651ec837654707
Eduardo Campaña <asrielo@gmail.com>
2018-05-16 17:55:37 +0200

    Adds title and content to Credits

--- src/pwa/components/Credits/index.js
+++ src/pwa/components/Credits/index.js
@@ -1,19 +1,40 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { inject } from 'mobx-react';
 import styled from 'react-emotion';
 import TopBar from '../TopBar';
 
-const Credits = () => (
+const Credits = ({ title, content }) => (
   <Fragment>
-    <Content>Credits</Content>
+    <Container>
+      <Title>{title}</Title>
+      <Content>{content}</Content>
+    </Container>
     <TopBar />
   </Fragment>
 );
 
-export default Credits;
+Credits.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
 
-const Content = styled.div`
+export default inject(({ connection }) => ({
+  title: connection.selectedItem.entity.title,
+  content: connection.selectedItem.entity.content,
+}))(Credits);
+
+const Container = styled.div`
   box-sizing: border-box;
   width: 100vw;
-  min-height: 100vh;
+  height: 100vh;
   padding: ${({ theme }) => theme.paddings.credits};
+`;
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Content = styled.section`
+  padding: 0 15px;
 `;
