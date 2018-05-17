a37cdbbc7a4f46cbea211c0302f50271ee0ab7c5
Luis Herranz <luisherranz@gmail.com>
2018-05-17 10:52:07 +0200

    Refactor server flow to mst

--- src/pwa/flows/server.js
+++ src/pwa/flows/server.js
@@ -1,6 +1,4 @@
-import { flow, getEnv, addMiddleware } from 'mobx-state-tree';
-import { when } from 'mobx';
-import { dep } from 'worona-deps';
+import { flow, addMiddleware } from 'mobx-state-tree';
 import { homeContext, venueContext, announcementsContext, creditsContext } from '../contexts';
 
 const extractId = href => /\/(\d+)$/g.exec(href)[1];
@@ -38,10 +36,6 @@ export default self =>
   flow(function* ThemeServer({ selectedItem }) {
     addMiddleware(self, sessionMiddleware);
 
-    const { store } = getEnv(self);
-    const routeChangeSucceed = dep('connection', 'actions', 'routeChangeSucceed');
-    const customRequested = dep('connection', 'actions', 'customRequested');
-
     const { type, id } = selectedItem;
     const action = { selectedItem: { type, id } };
 
@@ -55,35 +49,22 @@ export default self =>
       action.context = homeContext;
     }
 
-    store.dispatch(routeChangeSucceed(action));
+    self.connection.routeChangeSucceed(action);
 
-    // Request all sessions:
-    store.dispatch(
-      customRequested({
+    yield Promise.all([
+      self.connection.fetchCustomPage({
         custom: { name: 'sessions', type: 'wcb_session', page: 1 },
         params: { per_page: 100, _embed: false },
       }),
-    );
-
-    // Request all speakers:
-    store.dispatch(
-      customRequested({
+      self.connection.fetchCustomPage({
         custom: { name: 'speakers', type: 'wcb_speaker', page: 1 },
         params: { per_page: 100, _embed: false },
       }),
-    );
-
-    // Request all tracks:
-    store.dispatch(
-      customRequested({
+      self.connection.fetchCustomPage({
         custom: { name: 'tracks', type: 'wcb_track', page: 1 },
         params: { per_page: 100, _embed: false },
       }),
-    );
-
-    // Request needed pages:
-    store.dispatch(
-      customRequested({
+      self.connection.fetchCustomPage({
         custom: { name: 'pages', type: 'page', page: 1 },
         params: {
           per_page: 100,
@@ -91,14 +72,5 @@ export default self =>
           include: '15, 17, 19, 23, 26, 28, 30, 32, 34, 36',
         },
       }),
-    );
-
-    yield when(
-      () =>
-        self.connection.entity(type, id).isReady &&
-        self.connection.custom('sessions').isReady &&
-        self.connection.custom('speakers').isReady &&
-        self.connection.custom('tracks').isReady &&
-        self.connection.custom('pages').isReady,
-    );
+    ]);
   });
