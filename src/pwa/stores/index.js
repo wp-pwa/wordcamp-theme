7f8e58902bbe0e4a053f3843903f311edc9a66aa
David Arenas <darensr@gmail.com>
2018-05-18 17:29:04 +0200

    Fixes onNow and upNext views in theme

--- src/pwa/stores/index.js
+++ src/pwa/stores/index.js
@@ -15,58 +15,56 @@ export default types
     tracksMap: types.optional(types.map(Track), {}),
     speakersMap: types.optional(types.map(Speaker), {}),
   })
-  .views(self => ({
-    get sessions() {
-      return values(self.sessionsMap);
-    },
-    get tracks() {
-      return values(self.tracksMap).sort((a, b) => a.id - b.id);
-    },
-    get speakers() {
-      return values(self.speakersMap);
-    },
-    session(id) {
-      return self.sessionsMap.get(id) || self.sessionsMap.get(id.toString());
-    },
-    track(id) {
-      return self.tracksMap.get(id) || self.tracksMap.get(id.toString());
-    },
-    speaker(id) {
-      return self.speakersMap.get(id) || self.speakersMap.get(id.toString());
-    },
-    sessionsOnNow(date) {
-      return self.tracks
-        .slice(1) // removes "Networking" track
-        .map(t => t.sessionOnNow(date))
-        .filter(
-          // filters sessions in multiple tracks that are not
-          // taking place in all of them (like "Lunch").
-          (session, index, sArray) =>
-            (session && sArray.filter(s => s === session).length === 1) ||
-            sArray.filter(s => s === session).length === sArray.length,
-        )
+  .views(self => {
+    const filterSessions = sessions =>
+      sessions
+        // filters sessions in multiple tracks that are not
+        // taking place in all of them (like "Lunch").
+        .filter((session, index, sArray) => {
+          const count = sArray.filter(s => s === session).length;
+          return !!session && (count === 1 || count === sArray.length);
+        })
+        // removes repetitions
         .reduce((all, s) => {
           if (!all.includes(s)) all.push(s);
           return all;
         }, []);
-    },
-    sessionsUpNext(date) {
-      return self.tracks
-        .slice(1) // removes "Networking" track
-        .map(t => t.sessionUpNext(date))
-        .filter(
-          // filters sessions in multiple tracks that are not
-          // taking place in all of them (like "Lunch").
-          (session, index, sArray) =>
-            (session && sArray.filter(s => s === session).length === 1) ||
-            sArray.filter(s => s === session).length === sArray.length,
-        )
-        .reduce((all, s) => {
-          if (!all.includes(s)) all.push(s);
-          return all;
-        }, []);
-    },
-  }))
+
+    return {
+      get sessions() {
+        return values(self.sessionsMap);
+      },
+      get tracks() {
+        return values(self.tracksMap).sort((a, b) => a.id - b.id);
+      },
+      get speakers() {
+        return values(self.speakersMap);
+      },
+      session(id) {
+        return self.sessionsMap.get(id) || self.sessionsMap.get(id.toString());
+      },
+      track(id) {
+        return self.tracksMap.get(id) || self.tracksMap.get(id.toString());
+      },
+      speaker(id) {
+        return self.speakersMap.get(id) || self.speakersMap.get(id.toString());
+      },
+      sessionsOnNow(date) {
+        return filterSessions(
+          self.tracks
+            .slice(1) // removes "Networking" track
+            .map(t => t.sessionOnNow(date)),
+        );
+      },
+      sessionsUpNext(date) {
+        return filterSessions(
+          self.tracks
+            .slice(1) // removes "Networking" track
+            .map(t => t.sessionUpNext(date)),
+        );
+      },
+    };
+  })
   .actions(self => ({
     createSession(session) {
       // Init tracks
