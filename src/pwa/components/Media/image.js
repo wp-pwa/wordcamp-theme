27ebee8140ea3f973d88221bb4ffcdde7242a9f8
David Arenas <darensr@gmail.com>
2018-05-22 13:58:11 +0200

    Fixes srcSet attribute in Image component

--- src/pwa/components/Media/image.js
+++ src/pwa/components/Media/image.js
@@ -15,22 +15,31 @@ Image.propTypes = {
   srcSet: PropTypes.string.isRequired,
 };
 
+// Returns true if width/height ratio of both objects are very, very close.
+// Used when computing the srcSet prop value.
+const sameRatio = ({ width: w1, height: h1 }, { width: w2, height: h2 }) =>
+  Math.abs(w1 / h1 - w2 / h2) < 0.01;
+
 export default inject(({ settings }, { entity }) => {
   const cdn = (settings.theme.cdn || {}).images;
   const originalPath = parse(entity.original.url).path;
   const src = cdn && originalPath ? `${cdn}${originalPath}` : entity.original.url;
+
+  const sizes = entity.sizes
+    .reduce((result, current) => {
+      if (sameRatio(current, entity.original)) result.push(current);
+      return result;
+    }, [])
+    .map(item => {
+      const { path } = parse(item.url);
+      const url = cdn && path ? `${cdn}${path}` : item.url;
+      return `${url} ${item.width}w`;
+    })
+    .join(', ');
+
   return {
     alt: entity.alt,
     src: entity.original.url,
-    srcSet:
-      entity.sizes
-        .map(item => {
-          const { path } = parse(item.url);
-          const url = cdn && path ? `${cdn}${path}` : item.url;
-          return `${url} ${item.width}w`;
-        })
-        .join(', ') || src
-        ? `${src} 100w`
-        : '',
+    srcSet: sizes || (src ? `${src} 100w` : ''),
   };
 })(Image);
