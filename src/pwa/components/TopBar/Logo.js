92fb80785bdb094b829d09d7af53458cc9a3926c
Eduardo Campaña <asrielo@gmail.com>
2018-06-08 11:56:06 +0200

    Adds link to TopBar logo
    https://github.com/frontity/wordcamp-theme/issues/87

--- src/pwa/components/TopBar/Logo.js
+++ src/pwa/components/TopBar/Logo.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { inject } from 'mobx-react';
 import styled from 'react-emotion';
+import Link from '../Link';
+import { homeContext } from '../../contexts';
+import { PAGE_HOME_ON_NOW } from '../../consts';
 
 const Logo = ({ src, srcSet }) => (
   <Container>
-    <Img alt="Wordcamp logo" src={src} srcSet={srcSet} />
+    <Link type="page" id={PAGE_HOME_ON_NOW} context={homeContext}>
+      <A>
+        <Img alt="Wordcamp logo" src={src} srcSet={srcSet} />
+      </A>
+    </Link>
   </Container>
 );
 
@@ -35,6 +42,13 @@ const Container = styled.div`
   box-sizing: border-box;
   width: ${({ theme }) => theme.size.button};
   height: ${({ theme }) => theme.size.button};
+`;
+
+const A = styled.a`
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+  height: 100%;
   padding: 12px 0 12px 16px;
 `;
 
